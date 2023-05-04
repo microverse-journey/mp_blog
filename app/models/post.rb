346e9b353830329078bc77190057f736fd1366b9
mrprotocoll <lekanvgbg@gmail.com>
2023-05-04 18:34:34 +0100

    Added post method to returns the 5 most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,9 @@ class Post < ApplicationRecord
     count = where(author_id: author_id).count(:author_id)
     User.find_by(author_id: author_id).update(posts_counter: counter + 1)
   end
+
+  # returns the 5 most recent comments for a given post.
+  def five_most_recent_comments(post_id)
+    Comment.where(post_id: post_id).order("created_at" :desc).limit(5)
+  end
 end
