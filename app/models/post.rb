ecf40bc7d9567f898e677a75ac60139c2b639708
mrprotocoll <lekanvgbg@gmail.com>
2023-05-04 18:10:20 +0100

    Added post method that returns the 3 most recent posts for a given user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: "User"
-    has_many :likes
-    has_many :comments
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  # returns the 3 most recent posts for a given user
+  def self.three_most_recent(author_id)
+    where(author_id: author_id).order("created_at": :desc).limit(3)
+  end
 end
