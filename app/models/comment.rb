5d0bf10aed90043cc436c8d5db9b42d8cc2dda74
mrprotocoll <lekanvgbg@gmail.com>
2023-05-04 18:41:04 +0100

    Added comment method that updates the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  # updates the comments counter for a post
+  def update_comment_counter(post_id)
+    count = where(post_id: post_id).count(:post_id)
+    Post.find(post_id).update(:comments_counter count)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   #   updates the posts counter for a user.
   def self.update_counter(author_id)
     count = where(author_id: author_id).count(:author_id)
-    User.find_by(author_id: author_id).update(posts_counter: counter + 1)
+    User.find_by(author_id: author_id).update(posts_counter: counter)
   end
 
   # returns the 5 most recent comments for a given post.
