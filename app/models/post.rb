e924e4e408b47d94cb5e0f56091e85e7bea10813
mrprotocoll <lekanvgbg@gmail.com>
2023-05-05 20:53:35 +0100

    Added test for user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :comments
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :likes_counter, numericality: {greater_than_or_equal_to: 0}
-  validates :comments_counter, numericality: {greater_than_or_equal_to: 0}
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
 
   after_create :update_user_posts_counter
   after_destroy :update_user_posts_counter
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: {greater_than_or_equal_to: 0}
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   # returns the 3 most recent posts for a given user
-  def self.three_most_recent
+  def three_most_recent_post
     posts.order(created_at: :desc).limit(3)
   end
 end
