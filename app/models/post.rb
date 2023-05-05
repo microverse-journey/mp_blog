fec278161593e46ac4c15e56cde8508c48719319
mrprotocoll <lekanvgbg@gmail.com>
2023-05-05 19:19:40 +0100

    validated post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  validates :author, presence: true
-  validates :text, presence: true
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :likes_counter, numericality: {greater_than_or_equal_to: 0}
+  validates :comments_counter, numericality: {greater_than_or_equal_to: 0}
 
   after_create :update_user_posts_counter
   after_destroy :update_user_posts_counter
