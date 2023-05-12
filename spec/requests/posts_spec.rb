e20fe9edf1806dc3b979a652db6d4e22f6f0f470
mrprotocoll <lekanvgbg@gmail.com>
2023-05-12 11:11:32 +0100

    Fixed Linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'User Posts', type: :request do
-    describe "GET /users/:id/posts" do
-        it "should return http success" do
-            get "/users/1/posts"
-            expect(response.status).to eq(200)
-            expect(response).to have_http_status(:success)
-            expect(response).to render_template(:index)
-            expect(response.body).to include('All User Posts')
-        end
+  describe 'GET /users/:id/posts' do
+    it 'should return http success' do
+      get '/users/1/posts'
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
+      expect(response.body).to include('All User Posts')
     end
+  end
 
-    describe "GET /users/:id/posts/id" do
-        it "should return http success" do
-            get "/users/1/posts/1"
+  describe 'GET /users/:id/posts/id' do
+    it 'should return http success' do
+      get '/users/1/posts/1'
 
-            expect(response.status).to eq(200)
-            expect(response).to have_http_status(:success)
-            expect(response).to render_template(:show)
-            expect(response.body).to include('User Single Post')
-        end
+      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include('User Single Post')
     end
+  end
 end
