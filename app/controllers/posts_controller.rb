cfe1c91e82fb0921ab6bb0c0364f936cf01f9f4a
mrprotocoll <lekanvgbg@gmail.com>
2023-05-13 09:53:47 +0100

    updated post controller to include new and create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(
+      author_id: current_user.id,
+      title: params['post']['title'],
+      text: params['post']['text']
+    )
+
+    redirect_to user_post_path(current_user, @post)
+  end
 end
