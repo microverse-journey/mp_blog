47bc8d5403f0485434c51eb4d2b06282ad056fd2
mrprotocoll <lekanvgbg@gmail.com>
2023-05-13 11:56:15 +0100

    Added new comment feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,11 @@ class CommentsController < ApplicationController
   
     def create
       @comment = Comment.new(
-        post_id: params[:id],
+        post_id: params[:post_id],
         author_id: current_user.id,
         text: params['comment']['text']
       )
-  
+
       if(@comment.save)
         flash[:success] = 'Comment created successfully'
         redirect_to user_post_path(current_user, @comment.post)
