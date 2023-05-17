d04c77948cb7ff63d72f18fe3154aca88181d63a
mrprotocoll <lekanvgbg@gmail.com>
2023-05-17 09:01:51 +0100

    pulled and fixed n+1 problem

--- spec/system/posts/show_spec.rb
+++ spec/system/posts/show_spec.rb
@@ -16,29 +16,35 @@ RSpec.describe 'Posts', type: :system do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_content(@user.name)
     end
+
     it "shows the post's title" do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_text(@post.title)
     end
+
     it 'shows how many comments it has' do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_text(@post.comments_counter)
       expect(page).to have_selector('.comments-likes', count: 1)
     end
+
     it 'shows how many likes it has' do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_text(@post.likes_counter)
       expect(page).to have_selector('.comments-likes', count: 1)
     end
+
     it "shows the post's body" do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_text(@post.text)
     end
+    
     it 'shows the username of each commentor.' do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_content(@comment.author.name)
       expect(page).to have_content(@comment2.author.name)
     end
+
     it 'shows the comment each commentor left' do
       visit user_post_path(@user.id, @post.id)
       expect(page).to have_text(@comment.text)
