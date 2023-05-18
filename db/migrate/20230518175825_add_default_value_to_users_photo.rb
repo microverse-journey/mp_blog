037c509fad81b43671e7b185c5b81fe77d95b785
mrprotocoll <lekanvgbg@gmail.com>
2023-05-18 19:02:17 +0100

    Add migration

---
+++ db/migrate/20230518175825_add_default_value_to_users_photo.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToUsersPhoto < ActiveRecord::Migration[7.0]
+  def change
+    change_column :users, :photo, default: 'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o='
+  end
+end
