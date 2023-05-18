11cf5cdbf10f178e0b57e88038e21da358e8a754
mrprotocoll <lekanvgbg@gmail.com>
2023-05-18 08:44:18 +0100

    Add login and signup design

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :validatable, :confirmable, :rememberable
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 
   # returns the 3 most recent posts for a given user
   def three_most_recent_post
